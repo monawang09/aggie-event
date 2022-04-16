3f223907885989df83652f4e7458d64f929ee041
Yuxin Ren <yxren@ucdavis.edu>
2022-04-16 11:15:41 -0700

    search_bar.js changed

--- src/component/search_bar.js
+++ src/component/search_bar.js
@@ -30,35 +30,36 @@ const searchBar = () => {
   { name: "Portugal", continent: "Europe" },
   { name: "Pakistan", continent: "Asia" },
 ];
-const handleChange = (e) => {
-  e.preventDefault();
-  setSearchInput(e.target.value);
-};
-if (searchInput.length > 0) {
-    countries.filter((country) => {
-    return country.name.match(searchInput);
-});
-}
-return <div>
-<input
-   type="search"
-   placeholder="Search here"
-   onChange={handleChange}
-   value={searchInput} />
-<table>
-  <tr>
-    <th>Country</th>
-    <th>Continent</th>  
-  </tr>
-{countries.map((country, index) => {
-<div>
-  <tr>
-    <td>{country.name}</td>
-    <td>{country.continent}</td>
-  </tr>
-</div>
-})}
-</table>
-</div>
+    const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+    };
+
+    if (searchInput.length > 0) {
+        countries.filter((country) => {
+        return country.name.match(searchInput);
+        });
+    }
+    return <div>
+    <input
+        type="search"
+        placeholder="Search here"
+        onChange={handleChange}
+        value={searchInput} />
+        <table>
+            <tr>
+            <th>Country</th>
+            <th>Continent</th>
+            </tr>
+            {countries.map((country, index) => {
+            <div>
+                <tr>
+                <td>{country.name}</td>
+                <td>{country.continent}</td>
+                </tr>
+            </div>
+            })}
+        </table>
+    </div>
 };
 export default searchBar;
