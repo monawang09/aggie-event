000ce294c28084ba41e48ecb636eae22d3c6049e
monawang09 <wangtangbaihe@163.com>
2022-04-16 12:33:05 -0700

    cover-page added

--- src/component/cover-page.js
+++ src/component/cover-page.js
@@ -1,9 +1,24 @@
 import "./cover-page.css";
+import circle from './images/Ellipse3.svg'; 
+import hero_image from './images/Ellipse 1.svg'; 
+import minorHeroImage from './images/Ellipse 4.svg'; 
+import minoroverlay from './images/Ellipse 5.svg'; 
+
+// import logo from '.././logo.png';
+// import homepage from './images/homepage_hero.jpg';
 
 function HomePage() {
   return (
     <div>
-      <div className="title-container">NEW EVENTS HELLO</div>
+      <div>
+         {/* <img src={centerCircle} className='.centerCircle' /> */}
+         <div className="circle"></div>
+         <img src={circle} className='image-container' />
+         <img src={hero_image} className='image-container' />
+         <img src={minoroverlay} className='minorHeroImage' />
+         <img src={minorHeroImage} className='minorHeroImage' />
+      </div>
+      <div className="title-container">NEW EVENTS </div>
       <div className="subtitle-container">  To Spice Up Your Quarter </div>
       <div className="content-container"> 
       Lorem ipsum dolor sit amet,
@@ -11,6 +26,8 @@ function HomePage() {
       labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
       nostrud exercitation ullamco laboris.
       </div>
+      <div className="buttonText">Find Out More</div>
+      <div className="button"> </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from './component/cover-page'; 
@@ -8,7 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HomePage> </HomePage>
-   
   </React.StrictMode>
 );
 
