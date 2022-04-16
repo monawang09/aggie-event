fabf577a50d231b3b3bb728792df4cc6b40e21a9
monawang09 <wangtangbaihe@163.com>
2022-04-16 10:02:22 -0700

    Add HomePage

---
+++ src/component/cover-page.js
@@ -0,0 +1,18 @@
+import "./cover-page.css";
+
+function HomePage() {
+  return (
+    <div>
+      <div className="title-container">NEW EVENTS HELLO</div>
+      <div className="subtitle-container">  To Spice Up Your Quarter </div>
+      <div className="content-container"> 
+      Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+      nostrud exercitation ullamco laboris.
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import HomePage from './component/cover-page'; 
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HomePage> </HomePage>
+   
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
